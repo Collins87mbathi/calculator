820f82bac776f15e7a99f14ff200a6f1c0c88214
collins mbathi <perezcoolins@gmail.com>
2021-07-05 15:23:56 +0300

    created files for creating a pwa  and modified the manifest json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './App';
 
 
@@ -11,3 +12,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
